47cb7bf8a7aad91601eb0b9623593429c31ee10d
skarago <skarago81@gmail.com>
2015-09-19 08:57:23 +0300

    User authentication and login

--- SimpleCMS/App_Code/WebUser.cs
+++ SimpleCMS/App_Code/WebUser.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.Web.Helpers;
+using System.Web.SessionState;
 namespace SimpleCMS.App_Code
 {
     public class WebUser
     {
+        private static HttpSessionState Session
+        {
+            get { return HttpContext.Current.Session; }
+        }
+
         public static IEnumerable<string> GetRolesForUser(int id)
         {
             return RoleRepository.GetRolesForUser(id)
@@ -14,6 +20,101 @@ namespace SimpleCMS.App_Code
 
         }
 
+        public static bool Authenticate(string username, string password)
+        {
+            var user = AccountRepository.Get(username);
+            if (user == null)
+            {
+                return false;
+            }
+            return Crypto.VerifyHashedPassword((string)user.Password, password);
+
+        }
+
+        public static void Login(string username)
+        {
+            var user = AccountRepository.Get(username);
+            if (user == null)
+            {
+                return;
+            }
+
+            SetupSession(user);
+
+        }
+
+        public static bool AuthenticateAndLogin(string username, string password)
+        {
+            var user = AccountRepository.Get(username);
+            if (user == null)
+            {
+                return false;
+            }
+            var hashPassword = Crypto.HashPassword(password);
+
+            var verified = Crypto.VerifyHashedPassword(hashPassword, password);
+            
+            if (!verified)
+            {
+                return false;
+            }
+            SetupSession(user);
+            return true;
+
+        }
+
+        private static void SetupSession(dynamic user)
+        {
+            
+            Session["userid"] = (int)user.Id;
+            Session["username"] = (string)user.Username;
+            Session["email"] = (string)user.Email;
+        }
+
+        public static int UserId
+        {
+            get
+            {
+                var value = Session["userId"];
+                if (value == null)
+                {
+                    return -1;
+                }
+                return (int)value;
+            }
+        }
+        public static string UserName
+        {
+            get
+            {
+                var value = Session["UserName"];
+                if (value == null)
+                {
+                    return string.Empty;
+                }
+                return (string) value;
+            }
+        }
+
+        public static string Email
+        {
+            get
+            {
+                var value = Session["Email"];
+                if (value == null)
+                {
+                    return string.Empty;
+                }
+                return (string)value;
+            }
+        }
+        public static bool IsAuthenticated
+        {
+            get
+            {
+                return !String.IsNullOrEmpty(UserName);
+            }
+        }
     }
 
 
